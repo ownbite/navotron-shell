1412deb2082e7ef5e6363817f44906c07dda472e
Linus Wallin <linuswallin87@gmail.com>
2014-06-09 11:36:47 +0200

    Fixed so that npm, bower and http-server is installed correclty under /usr/local/bin/

--- puppet/modules/webb/manifests/grunt-settings.pp
+++ puppet/modules/webb/manifests/grunt-settings.pp
@@ -18,14 +18,39 @@ class webb::grunt-settings(
         source => "puppet:///modules/webb/node-v0.10.28-linux-x86.tar.gz",
     }
 
-    # Unpack Python 3.4
-    exec { "unpack-npm":
+    # Unpack npm
+    exec { "install-npm":
         path        => $system_paths,
-        command     => "tar -xzf node-v0.10.28-linux-x86.tar.gz",
-        cwd         => "/tmp/",
+        command     => "tar --strip-components 1 -xzf /tmp/node-v0.10.28-linux-x86.tar.gz",
+        cwd         => "/usr/local",
         logoutput   => on_failure,
+        creates     => [ "/usr/local/bin/npm" ],
         require     => File["/tmp/node-v0.10.28-linux-x86.tar.gz"],
     }
+
+    # Install bower
+    exec { "install-bower":
+        path        => $system_paths,
+        command     => "npm install -g bower",
+        # cwd         => "/usr/local",
+        logoutput   => on_failure,
+        creates     => [ "/usr/local/bin/bower" ],
+        require     => Exec["install-npm"],
+    }
+
+    # Install http-server
+    exec { "install-http-server":
+        path        => $system_paths,
+        command     => "sudo npm install -g http-server",
+        # cwd         => "/usr/local",
+        logoutput   => on_failure,
+        creates     => [ "/usr/local/bin/http-server" ],
+        require     => Exec["install-npm"],
+    }
+
+    
+    
+
 
 
 }
